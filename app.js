f92da84ded03aed5de17bee9b43ef5277b099327
Christian <christianleyva@vilop.com>
2019-09-10 19:58:47 -0400

    Ejercicio 3 - Correcion Param (nombre)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hola " + (req.query.name ? req.query.name : "desconocido")+"!</h1>");   
+    res.send("<h1>Hola " + (req.query.nombre ? req.query.nombre : "desconocido")+"!</h1>");   
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
