d0a9e2d7a2c751e06936570ba4096da5f21e2acb
Christian <christianleyva@vilop.com>
2019-09-10 21:13:47 -0400

    Ejercicio 6

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/:param', (req, res) => {
-    res.send(req.header(req.params.param));
+    res.send(req.header('User-Agent'));
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
