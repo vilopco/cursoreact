d38a0d585271abb4cb9d68005c0c418c17a0e6d8
Christian <christianleyva@vilop.com>
2019-09-10 20:09:34 -0400

    Ejercicio 4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hola " + (req.query.nombre ? req.query.nombre : "desconocido")+"!</h1>");   
+app.get('/makers/:nombre', (req, res) => {
+    res.send("<h1>Hola " + req.params.nombre[0].toLocaleUpperCase()+ req.params.nombre.slice(1)+"!</h1>");   
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
