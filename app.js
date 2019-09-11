38b92fb3f48e132d3700f501074688bd5310f19e
Christian <christianleyva@vilop.com>
2019-09-10 20:31:58 -0400

    Ejercicio 6

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+var bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    var limite=50;
-    var cadena="";
-    for(var i=1;i<=limite;i++){
-        cadena += (i%2==0 ? "<p>"+i+" Soy Par!</p>": "<p>"+i+" Soy Impar!</p>");
-    }
-    res.send(cadena);
+    var pagina= '<form method="post" action="//localhost:3000"><input type="text" name="nombre"><input type="submit" value="Submit"></form>';
+    res.send(pagina);
 });
+
+app.post('/', function (req, res) {
+    res.send("<h1>Hola "+req.body.nombre+"!</h1>");   
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
